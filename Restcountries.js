aa0c0404a45f37b0c20752a2889ca0ddf2b833d3
vicsurya19 <50830848+vicsurya19@users.noreply.github.com>
2020-02-27 11:52:15 +0530

    Add files via upload

---
+++ Restcountries.js
@@ -0,0 +1,13 @@
+var request = new XMLHttpRequest()
+request.open('GET','https://restcountries.eu/rest/v2/all',true)
+request.onload = function() 
+{
+    var data = JSON.parse(this.response)
+    for(var i in data)
+    {
+            console.log(data[i].name +":"+ data[i].alphacode)
+    }        
+}
+request.send()
+
+
